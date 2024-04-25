f7027e239ab913f2cf6129d8f5dc28c321a4aa67
HeriRandri <hrandri30@gmail.com>
2024-04-25 11:45:24 +0300

    modif

--- main.js
+++ main.js
@@ -1,9 +1,53 @@
-const rm = (str) => {
-  let ponctuation = /[.,\/#!$%?\^&\*;:{}=\-_`~()]/g;
-  let resultat = str.split(/\s+/).join(" ").replace(ponctuation, "");
-  return resultat;
-};
+"use strict";
 
-let string =
-  " Supp7rimez  les ponctuations3, # espaces et nombres d’une1 phrase donnée.? ";
-console.log(rm(string).replace(/[0-9]/g, ""));
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+  let count = 0;
+  for (let array of objects) {
+    if (array.x == array.y) count++;
+  }
+  return count;
+}
+
+function main() {
+  const n = +readLine();
+  let objects = [];
+
+  for (let i = 0; i < n; i++) {
+    const [a, b] = readLine().split(" ");
+
+    objects.push({ x: +a, y: +b });
+  }
+
+  console.log(getCount(objects));
+}
